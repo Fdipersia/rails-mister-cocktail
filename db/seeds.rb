b5c8e77664a1db2ac52086645d44645464b48252
Francisco Di Persia <fdipersia@gmail.com>
2018-11-09 17:10:06 -0300

    Iamges add

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,14 @@ ingredients.each do |ingredient|
   Ingredient.create(name: "#{ingredient.values}")
 end
 
-Cocktail.create(name:"Caipirinha")
-Cocktail.create(name:"Fernet")
-Cocktail.create(name:"Pisco")
+caipirinha = Cocktail.new(name:"Caipirinha")
+caipirinha.remote_photo_url = "https://bebidascoctel.com/wp-content/uploads/2018/10/caipirinha-900x500.jpeg"
+caipirinha.save
+
+fernet = Cocktail.new(name:"Fernet")
+fernet.remote_photo_url = "http://k30.kn3.net/7/2/F/D/8/2/101.png"
+fernet.save
+
+pisco = Cocktail.new(name:"Pisco sour")
+pisco.remote_photo_url = "http://dsijlpx1qzaz8.cloudfront.net/wp-content/uploads/2017/05/29195957/Pisco1.jpg"
+pisco.save
